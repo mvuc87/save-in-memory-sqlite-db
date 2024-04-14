2911925ee6d9ed7fac67b6b7efcc9ca3444af7cb
Milos Vuckovic <mvuc87@gmail.com>
2024-04-14 16:19:32 +0200

    init

---
+++ main.ts
@@ -0,0 +1,30 @@
+import { Database } from 'sqlite3';
+import { promisify } from 'util';
+
+const filepath = "C:\\temp\\storage-file";
+
+const createContactsTableSql = `
+  CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL
+  )
+`;
+
+const insertContactsSql = `
+  INSERT INTO
+    contacts (name, email)
+  VALUES
+    ('John Smith', 'john.smith@example.com'),
+    ('Jane Doe', 'jane.doe@imaginary.com')
+`;
+
+const vacuumSql = `VACUUM MAIN INTO "${filepath}"`;
+
+(async function main() {
+  const db = new Database(':memory:');
+  const exec = promisify(db.exec.bind(db));
+  await exec(createContactsTableSql);
+  await exec(insertContactsSql);
+  await exec(vacuumSql);
+})();
